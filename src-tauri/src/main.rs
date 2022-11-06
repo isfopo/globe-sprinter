d71afd83b5fa97ebf4cddc1637bffe0be18b18f8
Isaac Poole <isaac@switcherstudio.com>
2022-11-05 23:40:26 -0400

    declare closures

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -12,65 +12,71 @@ use config::{get_config, get_config_path};
 use menu::generate_menu;
 
 use std::process::Command;
-use tauri::{ClipboardManager, Manager, SystemTray, SystemTrayEvent};
+use tauri::{App, AppHandle, ClipboardManager, Manager, RunEvent, SystemTray, SystemTrayEvent};
 
 fn main() {
+    let setup = |app: &mut App| {
+        Ok({
+            let handle = app.handle();
+
+            let config = get_config(get_config_path(&app.handle()));
+
+            SystemTray::new()
+                .with_id("main")
+                .with_menu(generate_menu(config.as_object().unwrap()))
+                .build(&handle)
+                .expect("unable to create tray");
+        })
+    };
+
+    let system_tray_event = |app: &AppHandle, event: SystemTrayEvent| match event {
+        SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+            "config" => {
+                Command::new("open")
+                    .arg(get_config_path(app))
+                    .output()
+                    .expect("failed to execute process");
+            }
+            "open" => {
+                if let Some(window) = app.get_window("main") {
+                    window.show().unwrap();
+                } else {
+                    println!("no window");
+                }
+            }
+            "hide" => {
+                let window = app.get_window("main").unwrap();
+                window.hide().unwrap();
+            }
+            "quit" => {
+                std::process::exit(0);
+            }
+            id => {
+                app.clipboard_manager()
+                    .write_text(id)
+                    .expect("failed to copy");
+
+                Command::new("open")
+                    .arg("/bin/zsh")
+                    .output()
+                    .expect("failed to execute process");
+            }
+        },
+        _ => {}
+    };
+
+    let run = |_app_handle: &AppHandle, event: RunEvent| match event {
+        tauri::RunEvent::ExitRequested { api, .. } => {
+            api.prevent_exit();
+        }
+        _ => {}
+    };
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
-        .setup(|app| {
-            Ok({
-                let handle = app.handle();
-
-                let config = get_config(get_config_path(&app.handle()));
-
-                SystemTray::new()
-                    .with_id("main")
-                    .with_menu(generate_menu(config.as_object().unwrap()))
-                    .build(&handle)
-                    .expect("unable to create tray");
-            })
-        })
-        .on_system_tray_event(|app, event| match event {
-            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
-                "config" => {
-                    Command::new("open")
-                        .arg(get_config_path(app))
-                        .output()
-                        .expect("failed to execute process");
-                }
-                "open" => {
-                    if let Some(window) = app.get_window("main") {
-                        window.show().unwrap();
-                    } else {
-                        println!("no window");
-                    }
-                }
-                "hide" => {
-                    let window = app.get_window("main").unwrap();
-                    window.hide().unwrap();
-                }
-                "quit" => {
-                    std::process::exit(0);
-                }
-                id => {
-                    app.clipboard_manager()
-                        .write_text(id)
-                        .expect("failed to copy");
-
-                    Command::new("open")
-                        .arg("/bin/zsh")
-                        .output()
-                        .expect("failed to execute process");
-                }
-            },
-            _ => {}
-        })
+        .setup(setup)
+        .on_system_tray_event(system_tray_event)
         .build(tauri::generate_context!())
         .expect("error while building tauri application")
-        .run(|_app_handle, event| match event {
-            tauri::RunEvent::ExitRequested { api, .. } => {
-                api.prevent_exit();
-            }
-            _ => {}
-        });
+        .run(run);
 }
