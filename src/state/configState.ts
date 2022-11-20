d11032c8233c60040903f77c7c5a8f24e9d823c0
Isaac Poole <isaac@switcherstudio.com>
2022-11-20 01:55:23 -0500

    removes log

--- src/state/configState.ts
+++ src/state/configState.ts
@@ -9,7 +9,6 @@ export const configState = selector<Config | undefined>({
     return JSON.parse(await invoke<string>("get_config_json"));
   },
   set: ({ set }, config) => {
-    console.log(config);
     invoke("write_config", { json: JSON.stringify(config) });
   },
 });
