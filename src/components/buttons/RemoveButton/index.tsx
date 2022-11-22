0956d91fbc614a541b44ffc08d253c5e4687b5e4
Isaac Poole <isaac@switcherstudio.com>
2022-11-21 19:25:11 -0500

    styles remove button

--- src/components/buttons/RemoveButton/index.tsx
+++ src/components/buttons/RemoveButton/index.tsx
@@ -1,9 +1,14 @@
 import { ReactComponent as Remove } from "../../../assets/icons/remove.svg";
 import { useModalDispatch } from "../../../hooks/useModalDispatch";
 import { ButtonProps } from "../types";
+import styles from "./index.module.scss";
 
 export const RemoveButton: React.FC<ButtonProps> = ({ location }) => {
   const openModal = useModalDispatch();
 
-  return <Remove />;
+  return (
+    <button title="remove" className={styles["button"]}>
+      <Remove />
+    </button>
+  );
 };
