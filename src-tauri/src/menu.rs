6dd305f4ef981bc0ce3a0c0238c6e17e762be74c
Isaac Poole <isaac@switcherstudio.com>
2022-11-24 14:33:13 -0500

    reverses menu

--- src-tauri/src/menu.rs
+++ src-tauri/src/menu.rs
@@ -29,7 +29,7 @@ pub fn generate_menu_entry(
 ) -> SystemTrayMenuEntry {
     let mut submenu = SystemTrayMenu::new();
 
-    for (key, value) in directory.into_iter() {
+    for (key, value) in directory.into_iter().rev() {
         submenu.items.push(match value {
             ConfigItem::String(command) => {
                 SystemTrayMenuEntry::CustomItem(CustomMenuItem::new(command, key))
