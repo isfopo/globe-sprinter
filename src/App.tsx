7650439becccba688d31cf9de6a4c09a8bb8639a
Isaac Poole <isaac@switcherstudio.com>
2022-11-25 12:08:40 -0500

    adds error notification

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ConfigList } from "./components/lists/ConfigList";
 import { AddButton } from "./components/buttons/AddButton";
 import { Modals } from "./components/modals/Modals";
 import { useConfig } from "./hooks/useConfig";
+import { NotificationContainer } from "./components/NotificationContainer";
 
 export default () => {
   const { config, loading } = useConfig();
@@ -14,6 +15,7 @@ export default () => {
   return (
     <div className={styles["container"]}>
       <Modals />
+      <NotificationContainer />
       <div className={styles["menu"]}>
         <AddButton location="/" />
       </div>
---
+++ src/components/NotificationContainer.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import { listen } from "@tauri-apps/api/event";
+import "react-toastify/dist/ReactToastify.css";
+
+interface ErrorPayload {
+  message: string;
+}
+
+export const NotificationContainer = () => {
+  useEffect(() => {
+    const unlisten = listen<ErrorPayload>(
+      "error",
+      ({ payload: { message } }) => {
+        toast.error(message);
+      }
+    );
+
+    return () => {
+      unlisten.then((f) => f());
+    };
+  }, []);
+
+  return <ToastContainer theme="dark" />;
+};
