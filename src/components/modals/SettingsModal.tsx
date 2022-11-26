e468a72f6c0250505d23f3ee936e2faf6f98db20
Isaac Poole <isaac@switcherstudio.com>
2022-11-26 12:28:05 -0500

    creates settings class

--- src/components/modals/SettingsModal.tsx
+++ src/components/modals/SettingsModal.tsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 import { useModalListener } from "../../hooks/useModalListener";
 import { ModalBase } from "./ModalBase";
 import styles from "./index.module.scss";
+import { useSettings } from "../../hooks/useSettings";
 
 export const SettingsModal = () => {
+  const { settings, loading } = useSettings();
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const { close } = useModalListener(
     "settingsModal",
@@ -11,9 +13,13 @@ export const SettingsModal = () => {
     () => setIsOpen(false)
   );
 
+  if (loading) {
+    return <></>;
+  }
+
   return (
     <ModalBase isOpen={isOpen} onRequestClose={close}>
-      <p>settings</p>
+      <p>{settings?.shellPath}</p>
     </ModalBase>
   );
 };
--- src/hooks/useSettings.ts
+++ src/hooks/useSettings.ts
@@ -4,8 +4,13 @@ import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 import { settingsState } from "../state/settingsState";
 
-export interface Settings {
+export class Settings {
   shellPath: string;
+
+  constructor(json: string) {
+    const { shell_path } = JSON.parse(json);
+    this.shellPath = shell_path;
+  }
 }
 
 export const useSettings = (): {
@@ -17,9 +22,9 @@ export const useSettings = (): {
   useEffect(() => {
     const get = async () => {
       if (!settings) {
-        setSettings(JSON.parse(await invoke<string>("get_settings_json")));
+        setSettings(new Settings(await invoke<string>("get_settings_json")));
         await listen("reload", async () => {
-          setSettings(JSON.parse(await invoke<string>("get_settings_json")));
+          setSettings(new Settings(await invoke<string>("get_settings_json")));
         });
       }
     };
